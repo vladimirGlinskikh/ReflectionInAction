2eb445a6393374fad39f0e66d5e17f55e5b775f0
vladimirGlinskikh <support@zhelezyaka.kz>
2018-07-29 05:12:59 +0600

    configuration via AppConfig class

--- src/main/java/kz/zhelezyaka/spring/Main.java
+++ src/main/java/kz/zhelezyaka/spring/Main.java
@@ -1,13 +1,11 @@
 package kz.zhelezyaka.spring;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("kz.zhelezyaka\\context.xml");
-//        MessageRenderer renderer = (MessageRenderer) context.getBean("renderer");
-//        renderer.printMessage();
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
 
         IndependentMessageRenderer renderer = context.getBean(IndependentMessageRenderer.class);
         renderer.print();
