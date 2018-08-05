48f9c5982367907f8c9b179d125d0524a4fbccbe
Vladimir Glinskikh <support@zhelezyaka.kz>
2018-08-05 18:37:20 +0600

    Reflection in getters and setters with library lombok

--- src/main/java/kz/zhelezyaka/gettersAndSetters/Car.java
+++ src/main/java/kz/zhelezyaka/gettersAndSetters/Car.java
@@ -1,37 +1,17 @@
 package kz.zhelezyaka.gettersAndSetters;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Car {
     private String name;
     private Object price;
     private Object year;
 
-    public Car(String name, Object price, Object year) {
-        this.name = name;
-        this.price = price;
-        this.year = year;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Object getPrice() {
-        return price;
-    }
-
-    public void setPrice(Object price) {
-        this.price = price;
-    }
-
-    public Object getYear() {
-        return year;
-    }
-
-    public void setYear(Object year) {
-        this.year = year;
-    }
 }
